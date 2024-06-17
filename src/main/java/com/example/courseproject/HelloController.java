a782624aa6f7ef6b5237f547270202a7fd9939a8
valerie <13012005lera@gmail.com>
2024-06-17 03:24:32 +0300

    update_scene

--- src/main/java/com/example/courseproject/HelloController.java
+++ src/main/java/com/example/courseproject/HelloController.java
@@ -1,10 +1,20 @@
 package com.example.courseproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import org.w3c.dom.events.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
+
+    private ChoiceBox<String> role;
+
+
     @FXML
     private Label welcomeText;
 
@@ -12,16 +22,23 @@ public class HelloController {
     private Label welcomeText2;
 
 
-//    private Button onHelloButtonClick;
+    //    private Button onHelloButtonClick;
+//    @FXML
+////    void getRole(MouseEvent event) {
+////        role.getItems().addAll("Администратор", "Повар", "Официант");
+////    }
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Вы администратор!");
 //        onHelloButtonClick.setText("Вы кликнули!");
     }
+
     @FXML
     protected void onHelloButtonClick2() {
         welcomeText2.setText("Вы официант!");
 //        onHelloButtonClick.setText("Вы кликнули!");
     }
+
+
 }
